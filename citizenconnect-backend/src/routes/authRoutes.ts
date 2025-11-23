239b54e6214bf74a456bd7358f25bcefbdd98ba6
balrajmalusare02 <malusarebalraj02@gmail.com>
2025-11-23 13:37:41 +0530

    fix: Swagger Indentation errors

--- citizenconnect-backend/src/routes/authRoutes.ts
+++ citizenconnect-backend/src/routes/authRoutes.ts
@@ -1,10 +1,8 @@
-//Purpose:
-//Defines /api/auth/register and /api/auth/login endpoints.
-
+// Purpose:
+// Defines /api/auth/register and /api/auth/login endpoints.
 
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authController";
-//import { protect, authorize } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
@@ -50,9 +48,6 @@ router.post("/register", registerUser);
  * 401:
  * description: Invalid credentials
  */
-
 router.post("/login", loginUser);
-//router.get("/getUsers", getUsers);
 
 export default router;
-
